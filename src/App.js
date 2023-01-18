a2516d93943a798db50daf77f996d792b2fce214
yyyljy <yyyljy@gmail.com>
2023-01-19 00:49:20 +0900

    add img

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import trevi from "./assets/img/trevi.png";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <a>Learn React</a>
+        <img src={trevi} />
       </header>
     </div>
   );
